815fd6477f8f124da3054a9cc34811db1ef530d9
Green Java Dude <greenjavadude@gmail.com>
2015-07-15 17:19:55 +0200

    Update

--- MediaPlayer/src/com/greenjavadude/MediaPlayer/Test.java
+++ MediaPlayer/src/com/greenjavadude/MediaPlayer/Test.java
@@ -1,47 +1,5 @@
 package com.greenjavadude.MediaPlayer;
 
-import java.io.*;
-import java.net.URL;
-
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
-import javax.swing.*;
-
-public class Test extends Application{
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
-	public void start(Stage stage) throws Exception {
-		JFileChooser chooser = new JFileChooser();
-		int i = chooser.showDialog(null, "save");
-		URL url = chooser.getSelectedFile().toURI().toURL();
-		String path = url.getPath();
-		Scene scene = new Scene(new Group(), 800, 500);
-		stage.setScene(scene);
-		
-		if(path.endsWith(".mp4")){
-			Video media = new Video("title", new File(path));
-			VideoPlayer vp = new VideoPlayer(media, Mode.SINGLE);
-			((Group) scene.getRoot()).getChildren().add(vp.getMediaView());
-			vp.start();
-		}else{
-			Song media = new Song("title", new File(path));
-			MusicPlayer mp = new MusicPlayer(media, Mode.SINGLE);
-			mp.start();
-		}
-		
-		stage.show();
-	}
+public class Test {
+	//test out the other classes
 }
-
-
-
-
-
-
-
