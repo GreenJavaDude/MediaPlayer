de081987aacdd9e6f70978a2f6e057c053e66a29
Green Java Dude <greenjavadude@gmail.com>
2015-07-15 20:24:22 +0200

    Changed music player from javafx to javax

--- MediaPlayer/src/com/greenjavadude/MediaPlayer/Test.java
+++ MediaPlayer/src/com/greenjavadude/MediaPlayer/Test.java
@@ -1,5 +1,18 @@
 package com.greenjavadude.MediaPlayer;
 
+import java.io.File;
+
 public class Test {
-	
+	public static void main(String[] args){
+		Song song = new Song("Testsong", new File("C://Programming//test.wav"));
+		SongPlayer player = new SongPlayer(song);
+		player.start();
+		
+		try{
+			Thread.sleep(5000);
+		}catch(Exception e){
+			
+		}
+		player.stop();
+	}
 }
