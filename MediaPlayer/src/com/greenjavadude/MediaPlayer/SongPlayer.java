e02d2b4319253c4a913d0be3b37ef8b2ba7e16c3
Green Java Dude <greenjavadude@gmail.com>
2015-07-15 18:58:43 +0200

    Busy

---
+++ MediaPlayer/src/com/greenjavadude/MediaPlayer/SongPlayer.java
@@ -0,0 +1,73 @@
+package com.greenjavadude.MediaPlayer;
+
+import java.io.IOException;
+
+import javax.sound.sampled.*;
+import javax.sound.sampled.DataLine.Info;
+
+public class SongPlayer implements Runnable{
+	public static final int BYTE_BUFFER = 4096;
+	
+	private Song song;
+	private boolean running;
+	
+	private AudioInputStream stream;
+	private AudioFormat format;
+	private Info info;
+	private SourceDataLine line;
+	
+	public SongPlayer(Song s){
+		running = false;
+		song = s;
+	}
+	
+	public void run(){
+		try{
+			stream = AudioSystem.getAudioInputStream(song.getFile());
+			format = stream.getFormat();
+			info = new Info(SourceDataLine.class, format);
+			line = (SourceDataLine) AudioSystem.getLine(info);
+			
+			line.open(format);
+			
+			//starts playing
+			line.start();
+			
+			byte[] buffer = new byte[BYTE_BUFFER];
+			int bytesread = -1;
+			
+			while((running) && ((bytesread = stream.read(buffer)) != -1)){
+				line.write(buffer, 0, bytesread);
+			}
+			stop();
+		}catch(Exception e){
+			
+		}
+	}
+	
+	public void start(){
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public void stop(){
+		running = false;
+		line.drain();
+		line.close();
+		try{
+			stream.close();
+		}catch(IOException e){
+			
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
--- MediaPlayer/src/com/greenjavadude/MediaPlayer/Test.java
+++ MediaPlayer/src/com/greenjavadude/MediaPlayer/Test.java
@@ -1,5 +1,5 @@
 package com.greenjavadude.MediaPlayer;
 
 public class Test {
-	//test out the other classes
+	
 }
